fd68b4c270029d5c1af67e96f0ec11fdc9d639f4
trlinh31 <119785508+trlinh31@users.noreply.github.com>
2025-11-07 10:52:02 +0700

    Delete src/views/auth/Login directory
    delete folder

--- src/views/auth/Login/hooks/useLogin.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from '@tanstack/react-query'
-import { apiLogin } from '@/services/AuthService'
-import { LoginRequest } from '@/@types/auth'
-
-export const useLoginMutation = () => {
-  return useMutation({
-    mutationFn: (payload: LoginRequest) => apiLogin(payload),
-  })
-}
--- src/views/auth/Login/schemas/login.schema.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from 'yup'
-
-export const loginValidationSchema = Yup.object().shape({
-  username: Yup.string().required('Tên đăng nhập không được bỏ trống'),
-  password: Yup.string().required('Mật khẩu không được bỏ trống'),
-  rememberMe: Yup.bool(),
-})
