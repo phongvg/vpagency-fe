59b69c2ae7adfa625d0a5756af25310d141d1635
trlinh31 <119785508+trlinh31@users.noreply.github.com>
2025-11-07 10:52:11 +0700

    Delete src/views/auth/LoginTelegram directory
    delete folder

--- src/views/auth/LoginTelegram/hooks/useLoginTelegramMutation.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from '@tanstack/react-query'
-import { apiLoginTelegram } from '@/services/AuthService'
-
-export const useLoginTelegramMutation = (code: string) => {
-  return useMutation({
-    mutationFn: () => apiLoginTelegram(code),
-  })
-}
