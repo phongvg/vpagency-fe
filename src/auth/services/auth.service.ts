423342184e0cee3d46733ce85f6eeb3fdb961fae
Linh Tran <nguyentranlinh31@gmail.com>
2026-02-11 14:26:44 +0700

    remove code unused

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,15 +1,12 @@
 import { authApi } from "@/auth/api/auth.api";
 import type { LoginResponse } from "@/auth/types/auth.type";
 import { queryClient } from "@/configs/queryClient";
-import { ACCESS_TOKEN, EXPIRES_AT, FIVE_MINUTES_IN_MS, REFRESH_TOKEN, USER_ID } from "@/shared/constants/auth.constant";
+import { ACCESS_TOKEN, EXPIRES_AT, REFRESH_TOKEN, USER_ID } from "@/shared/constants/auth.constant";
 import { useAuthStore } from "@/shared/stores/auth/useAuthStore";
 import { getExpiresAtFromToken } from "@/shared/utils/jwt.util";
 import { getStorageItem, setStorageItem } from "@/shared/utils/storage.util";
 import Cookies from "js-cookie";
 import toast from "react-hot-toast";
-
-let refreshTimeoutId: number | null = null;
-let refreshPromise: Promise<void> | null = null;
 
 export const authService = {
   login: (data: LoginResponse): void => {
@@ -30,85 +27,17 @@ export const authService = {
     setUser(user);
     setAuthenticated(true);
     setLoading(false);
-
-    setTimeout(() => {
-      authService.scheduleRefresh();
-    }, 0);
   },
 
   setSession: (accessToken: string, refreshToken: string): void => {
     setStorageItem(ACCESS_TOKEN, accessToken);
-    // setStorageItem(REFRESH_TOKEN, refreshToken);
     Cookies.set(REFRESH_TOKEN, refreshToken, {
       secure: true,
       sameSite: "strict",
     });
   },
 
-  scheduleRefresh: () => {
-    if (refreshTimeoutId) clearTimeout(refreshTimeoutId);
-
-    const expiresAt = Number(getStorageItem<string | null>(EXPIRES_AT, null));
-    if (!expiresAt) return;
-
-    const target = expiresAt - FIVE_MINUTES_IN_MS;
-    const delay = target - Date.now();
-
-    if (delay <= 0) {
-      authService.refreshToken().catch((error) => {
-        console.error("Failed to refresh token:", error);
-        authService.logout();
-      });
-
-      return;
-    }
-
-    const MAX_TIMEOUT = 2_000_000_000;
-    const timeout = Math.min(delay, MAX_TIMEOUT);
-
-    refreshTimeoutId = window.setTimeout(() => {
-      authService.refreshToken().catch((error) => {
-        console.error("Failed to refresh token:", error);
-        authService.logout();
-      });
-    }, timeout);
-  },
-
-  refreshToken: async (): Promise<void> => {
-    if (!refreshPromise) {
-      const userId = getStorageItem<string | null>(USER_ID, null);
-
-      if (!userId) {
-        throw new Error("No user ID available");
-      }
-
-      refreshPromise = authApi
-        .refreshToken(userId)
-        .then((res) => {
-          const { accessToken, refreshToken } = res.data;
-          authService.setSession(accessToken, refreshToken);
-
-          const expiresAtMs = getExpiresAtFromToken(accessToken);
-          if (expiresAtMs) {
-            setStorageItem(EXPIRES_AT, expiresAtMs.toString());
-            authService.scheduleRefresh();
-          }
-        })
-        .catch((error) => {
-          console.error("Refresh token failed:", error);
-          authService.logout();
-          throw error;
-        })
-        .finally(() => {
-          refreshPromise = null;
-        });
-    }
-
-    return refreshPromise;
-  },
-
   clearSession() {
-    if (refreshTimeoutId) clearTimeout(refreshTimeoutId);
     localStorage.clear();
   },
 
