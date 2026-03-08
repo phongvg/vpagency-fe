5948bac1fb0881b8a71b78e53b671475afe1dcc9
Linh Dev <nguyentranlinh31@gmail.com>
2026-03-08 17:28:36 +0700

    refactor: remove unused code

--- src/modules/task/hooks/useUnassignCampaignMetrics.ts
+++ src/modules/task/hooks/useUnassignCampaignMetrics.ts
@@ -1,10 +1,8 @@
 import { taskApi } from "@/modules/task/api/task.api";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 export const useUnassignCampaignMetrics = () => {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: (taskId: string) => taskApi.unassignCampaignMetrics(taskId),
     onSuccess: (res) => {
