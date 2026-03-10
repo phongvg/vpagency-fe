f513591c362f02d2783a8df03b830ef227b77baf
Linh Tran <nguyentranlinh31@gmail.com>
2026-03-10 10:28:38 +0700

    feat: reorder InfoRow components in TaskCampaignCard for improved clarity

--- src/modules/task/components/TaskPanel/TaskCampaignCard.tsx
+++ src/modules/task/components/TaskPanel/TaskCampaignCard.tsx
@@ -16,10 +16,19 @@ export default function TaskCampaignCard({ task }: TaskCampaignCardProps) {
       </CardHeader>
 
       <CardContent className='space-y-2'>
-        <InfoRow label='Số lượng chiến dịch lên' value={task.numberOfCampaigns || 0} icon={<Megaphone className='w-4 h-4' />} />
-        <InfoRow label='Số lượng kết quả chiến dịch' value={task.numberOfResultCampaigns || 0} icon={<CheckCircle className='w-4 h-4' />} />
-        <InfoRow label='Ngân sách hàng ngày' value={formatDollarAmount(task.dailyBudget || 0)} icon={<DollarSign className='w-4 h-4' />} />
-        <InfoRow label='Số lượng tài khoản dự phòng' value={task.numberOfBackupCampaigns || 0} icon={<Shield className='w-4 h-4' />} />
+        {task.numberOfAccounts && <InfoRow label='Số lượng tài khoản' value={task.numberOfAccounts || 0} icon={<Megaphone className='w-4 h-4' />} />}
+        {task.numberOfCampaigns && (
+          <InfoRow label='Số lượng chiến dịch lên' value={task.numberOfCampaigns || 0} icon={<Megaphone className='w-4 h-4' />} />
+        )}
+        {task.numberOfResultCampaigns && (
+          <InfoRow label='Số lượng kết quả chiến dịch' value={task.numberOfResultCampaigns || 0} icon={<CheckCircle className='w-4 h-4' />} />
+        )}
+        {task.dailyBudget && (
+          <InfoRow label='Ngân sách hàng ngày' value={formatDollarAmount(task.dailyBudget || 0)} icon={<DollarSign className='w-4 h-4' />} />
+        )}
+        {task.numberOfBackupCampaigns && (
+          <InfoRow label='Số lượng tài khoản dự phòng' value={task.numberOfBackupCampaigns || 0} icon={<Shield className='w-4 h-4' />} />
+        )}
       </CardContent>
     </Card>
   );
